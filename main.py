fe338afb0d88964ba923727293b0fbbc255095a1
mohsenfayyaz <mohsen.fayyaz77@gmail.com>
2020-08-15 18:27:18 +0430

    Start sending data

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from mininet_script import Topology
 
 MIN_BW = 1
 MAX_BW = 5
+BW_INTERVAL = 10
+SEND_TCP_INTERVAL = 0.1
 
 running = True
 
@@ -31,9 +33,9 @@ def main():
     c0 = net.addController( 'c0', controller=RemoteController, ip='127.0.0.1', port=6633 )
 
     net.start()
-    threading.Thread(target=change_bw_timer_task,
-                     name='TIMER_TASK1', args=(net,)).start()
-    
+    threading.Thread(target=change_bw_timer_task, args=(net,)).start()
+    # threading.Thread(target=send_data_timer_task, args=(net,)).start()
+
     print ("*** Running CLI")
     CLI(net)  # Bring up the mininet CLI
     
@@ -43,6 +45,16 @@ def main():
     net.stop()
 
 
+def send_data_timer_task(net):  # runs every second
+    global running
+    while running:
+        # for host in net.hosts:
+            #target_host = random
+            # net.iperf( self, hosts=[host, ], l4Type='TCP', udpBw='100K', fmt=None,seconds=5, port=5001)
+        print("sent TCP messages: ")
+        time.sleep(SEND_TCP_INTERVAL)
+
+
 def change_bw_timer_task(net):  # runs every second
     global running
     while running:
@@ -50,7 +62,7 @@ def change_bw_timer_task(net):  # runs every second
             bw = random.randint(MIN_BW, MAX_BW)
             link.intf1.params["bw"] = bw
         print("net.links[0].intf1: ", net.links[0].intf1.params["bw"])
-        time.sleep(1)
+        time.sleep(BW_INTERVAL)
 
 
 if __name__ == '__main__':
