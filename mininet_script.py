322ed66a756cb2654208aed25d7e7ae4007cd0e4
zhivar <zhivarsourati@gmail.com>
2020-08-15 14:06:49 +0430

    Add complete topology without loops so it could work

--- mininet_script.py
+++ mininet_script.py
@@ -8,18 +8,18 @@ class CN_final(Topo):
         # hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
-        # h3 = self.addHost('h3')
-        # h4 = self.addHost('h4')
-        # h5 = self.addHost('h5')
-        # h6 = self.addHost('h6')
-        # h7 = self.addHost('h7')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        h7 = self.addHost('h7')
 
 
         # switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        # s3 = self.addSwitch('s3')
-        # s4 = self.addSwitch('s4')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
 
 
         # links
@@ -27,14 +27,14 @@ class CN_final(Topo):
         self.addLink(s2, s1)
         self.addLink(h2, s2)
         # self.addLink(s2, s3)
-        # self.addLink(s1, s3)
+        self.addLink(s1, s3)
         # self.addLink(s2, s4)
-        # self.addLink(h3, s3)
-        # self.addLink(h4, s3)
-        # self.addLink(s3, s4)
-        # self.addLink(h5, s4)
-        # self.addLink(h6, s4)
-        # self.addLink(h7, s4)
+        self.addLink(h3, s3)
+        self.addLink(h4, s3)
+        self.addLink(s3, s4)
+        self.addLink(h5, s4)
+        self.addLink(h6, s4)
+        self.addLink(h7, s4)
 
 
 topos = {'CN_final': (lambda : CN_final())}
