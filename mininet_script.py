1e8c09202075617f2491baf6f5b9394d6156b121
zhivar <zhivarsourati@gmail.com>
2020-08-15 13:26:38 +0430

    Add switches to script

--- mininet_script.py
+++ mininet_script.py
@@ -5,7 +5,7 @@ class topology(Topo):
     def __init__(self):
         Topo.__init__(self)
         
-
+        # hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
@@ -13,3 +13,10 @@ class topology(Topo):
         h5 = self.addHost('h5')
         h6 = self.addHost('h6')
         h7 = self.addHost('h7')
+
+
+        # switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
