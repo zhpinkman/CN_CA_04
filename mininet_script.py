9335a021309fe4c27312a22ff405cb140d5b32e2
zhivar <zhivarsourati@gmail.com>
2020-08-15 13:24:44 +0430

    Add hosts to script

---
+++ mininet_script.py
@@ -0,0 +1,15 @@
+from mininet.topo import Topo
+
+
+class topology(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        
+
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        h7 = self.addHost('h7')
